1f1e9195701360c8a42ecbc586376d61ed483477
12006967 <lambertmaxime.35000@gmail.com>
2020-01-22 09:41:01 +0100

    squelette basique pour le composant solfege

---
+++ MusicMaster/src/app/solfege/solfege.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solfege',
+  templateUrl: './solfege.component.html',
+  styleUrls: ['./solfege.component.scss'],
+})
+export class SolfegeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
