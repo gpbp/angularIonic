1a0372982aea084f033edc0c205821e305e18b39
trung <hai-trung.pham@etudiant.univ-rennes1.fr>
2020-01-24 14:45:49 +0100

    create login and sign up component

---
+++ MusicMaster/src/app/tab2/signUp/tab2.sign-up.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { samePasswordValidator } from './samePassword.directive';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: 'tab2.sign-up.component.html',
+  styleUrls: ['tab2.sign-up.component.scss']
+})
+export class SignUpComponent implements OnInit {
+  private error;
+  
+  constructor(public afAuth: AngularFireAuth, private router: Router) { }
+
+  ngOnInit() {
+    //this.setUpForm();
+  }
+  
+  private setUpForm() {
+    this.addValidatorToRetypedPwControl();
+  }
+
+  createAccount(formData) {
+    if (formData.valid) {
+      const email = formData.value.email;
+      const password = formData.value.password;
+      this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+      .catch((error) => this.error = error);
+    } 
+  }
+
+  private addValidatorToRetypedPwControl() {
+    //const passwordControl = this.createAccountForm.get('password');
+    //const password = passwordControl.value;
+    //this.createAccountForm.get('retyped_password').setValidators(samePasswordValidator(password));
+  }
+}
--- MusicMaster/src/app/tab2/tab2.page.ts
+++ MusicMaster/src/app/tab2/tab2.page.ts
@@ -1,7 +1,5 @@
+
 import { Component } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { Router } from '@angular/router';
-import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-tab2',
@@ -9,27 +7,7 @@ import { auth } from 'firebase/app';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  private error;
 
-  constructor(public afAuth: AngularFireAuth, private router: Router) {}
+  constructor() {}
 
-  loginWithFacebook() {
-    this.afAuth.auth.signInWithPopup(new auth.FacebookAuthProvider()).catch((error) => console.log(error));
-  }
-
-  loginWithGoogle() {
-    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider()).catch((error) => console.log(error));
-  }
-
-  loginWithEmailAndPassword(formData) {
-    if (formData.valid) {
-      this.afAuth.auth.signInWithEmailAndPassword(formData.value.email, formData.value.password)
-      .catch((error) => {
-        this.error = error;
-      });
-    }
-  }
-
-
-  
 }
