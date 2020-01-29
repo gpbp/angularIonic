0db5641c2079ac50a541c215774364849ee941ce
Rouzic Leo <leo.rouzic@etudiant.univ-rennes1.fr>
2020-01-29 10:05:09 +0100

    Final commit, export agenda new functionnality success, french page.

--- MusicMaster/src/app/agenda/agenda.module.ts
+++ MusicMaster/src/app/agenda/agenda.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-
-import {IonicModule} from '@ionic/angular';
-
+import {File} from '@ionic-native/file/ngx';
+import {AlertController, IonicModule, Platform} from '@ionic/angular';
 import {AgendaPageRoutingModule} from './agenda-routing.module';
 import {NgCalendarModule} from 'ionic2-calendar';
 import {AgendaPage} from './agenda.page';
+import {ExportData} from '../services/export_data';
 
 @NgModule({
   imports: [
@@ -17,6 +17,11 @@ import {AgendaPage} from './agenda.page';
     NgCalendarModule
   ],
 
-  declarations: [AgendaPage]
+  declarations: [AgendaPage],
+  providers: [
+    File,
+    AlertController,
+    Platform
+  ]
 })
 export class AgendaPageModule {}
--- MusicMaster/src/app/agenda/agenda.page.ts
+++ MusicMaster/src/app/agenda/agenda.page.ts
@@ -1,7 +1,10 @@
 import {Component, Inject, LOCALE_ID, OnInit, ViewChild} from '@angular/core';
 import {CalendarComponent} from 'ionic2-calendar/calendar';
 import {AlertController} from '@ionic/angular';
-import {formatDate} from '@angular/common';
+import {DatePipe, formatDate} from '@angular/common';
+import {DownloadFormat} from '../../models/download-format';
+import {File} from '@ionic-native/file/ngx';
+import {Platform} from '@ionic/angular';
 
 @Component({
   selector: 'app-agenda',
@@ -28,10 +31,11 @@ export class AgendaPage implements OnInit {
   };
 
   data = [];
-
   @ViewChild(CalendarComponent, null) myCal: CalendarComponent;
 
-  constructor(private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string) { }
+  // tslint:disable-next-line:max-line-length
+  constructor(private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string, private file: File, private platform: Platform) {
+  }
 
   ngOnInit() {
     this.resetEvent();
@@ -66,6 +70,7 @@ export class AgendaPage implements OnInit {
 
     this.eventSource.push(eventCopy);
     this.myCal.loadEvents();
+    this.data.push(eventCopy);
     this.resetEvent();
   }
   // Change current month/week/day
@@ -117,12 +122,121 @@ export class AgendaPage implements OnInit {
     this.event.endTime = (selected.toISOString());
   }
 
-
-
+  // Exportation of the calendar
   exportMyCalendar() {
-
+    this.export2CSV(this.data, DownloadFormat.COMMA, 'agenda.csv', this.myCal);
   }
 
+  /**
+   * Format the data from the calendarComponent to a data format adapted for
+   * the CSV
+   * @param myCal  The component from the Agenda page
+   * @param data Data to insert
+   * @return formated data for the csv file
+   */
+  private formatData(myCal: CalendarComponent, data: any[]): any[] {
+    const dateformat = new DatePipe(this.locale);
+    // Formating data
+    myCal.eventSource.forEach((value, index) => {
+      data[index] = {
+        Subject: value.title,
+        StartDate: dateformat.transform(new Date(value.startTime), 'MM/dd/yyyy'),
+        StartTime: dateformat.transform(new Date(value.startTime), 'HH:mm'),
+        EndDate: dateformat.transform(new Date(value.endTime), 'MM/dd/yyyy'),
+        EndTime: dateformat.transform(new Date(value.startTime), 'HH:mm'),
+        AllDayEvent: value.allDay
+      };
+    });
+    return data;
+  }
+
+  /**
+   * Function which is use to export data 2 csv file.
+   * @param any[] data Data which we want to download
+   * @param DownloadFormat columnDelimiter Delimiter of data file.
+   * @param string filename Name of file.
+   * @returns boolean True if downloaded successful, False otherwise.
+   */
+  public export2CSV(data: any[], columnDelimiter: DownloadFormat, filename: string, myCal: CalendarComponent): boolean {
+    if (!data || data.length === 0) {
+      console.log('false');
+
+      return false;
+    }
+    data = this.formatData(myCal, data);
+    console.log('Formatage fini : ', data);
+
+    const result = this.convertDataToCSV(data, columnDelimiter);
+    console.log('Conversion terminé ! : ', result);
+
+    this.downloadCSV(result, filename);
+    return true;
+  }
+
+  /**
+   * Function which is use to convert row data to csv string.
+   * @param any[] data Row data which we want to export
+   * @param DownloadFormat columnDelimiter Delimiter for CSV file.
+   * @returns string return csv data string.
+   */
+  private convertDataToCSV(data: any[], columnDelimiter: DownloadFormat = DownloadFormat.COMMA): string {
+
+    const lineDelimiter = '\n';
+    let fields: string[] = [];
+    for (const key in data[0]) {
+      if ('object' === typeof data[0][key]) {
+        const subFields: string[] = Object.keys(data[0][key]).map((k) => key + '.' + k);
+        fields = fields.concat(subFields);
+      } else {
+        fields.push(key);
+      }
+    }
+    let result = '';
+    result += fields.map((field) => field.toUpperCase()).join(columnDelimiter);
+    result += lineDelimiter;
+    data.forEach((item) => {
+      let ctr = 0;
+      fields.forEach((key) => {
+        if (ctr > 0) {
+          result += columnDelimiter;
+        }
+        if (item[key]) {
+          result += item[key];
+        } else {
+          const subKeys: string[] = key.split('.');
+          result += item[subKeys[0]][subKeys[1]];
+        }
+        ctr++;
+      });
+      result += lineDelimiter;
+    });
+    return result;
+  }
+
+  /**
+   * Function which is use to download CSV file of given data.
+   * @param string csvdata CSV data string which is going to download.
+   * @param string filename Name of file with extension.
+   */
+  private downloadCSV(csvdata: string, filename: string = 'agenda.csv') {
+    if (!csvdata.match(/^data:text\/csv/i)) {
+      csvdata = 'data:text/csv;charset=utf-8,' + csvdata;
+    }
+    if (this.platform.is('desktop')) {
+      const data: string = encodeURI(csvdata);
+      const link: HTMLElement = document.createElement('a');
+      link.setAttribute('href', data);
+      link.setAttribute('download', filename);
+      link.click();
+    } else {
+      const filedir = this.file.applicationDirectory;
+      return this.file.writeFile(filedir, filename, csvdata, {replace: true}).then(res => {
+        console.log('success');
+      }).catch(e => {
+        console.log('Share failed:', e);
+      });
+    }
+  }
 
 
 
